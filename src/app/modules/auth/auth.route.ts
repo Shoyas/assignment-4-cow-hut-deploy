b22d8f121ce8b1f7cf86ac986ac5e5f1f5c4b273
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2023-07-11 19:36:51 +0600

    [DONE] fixing route problem issue

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
+import { UserController } from '../users/user.controller';
+import { UserValidation } from '../users/user.validation';
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
 
 const router = express.Router();
+
+router.post(
+  '/signup',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser
+);
 
 router.post(
   '/login',
