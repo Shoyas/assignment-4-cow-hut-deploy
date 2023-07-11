adf9a70bfbdca6ec33b1f60401c7363492218b2a
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2023-07-11 19:54:22 +0600

    new dist file generated

--- dist/app/modules/users/user.route.js
+++ dist/app/modules/users/user.route.js
@@ -11,7 +11,13 @@ const validateRequest_1 = __importDefault(require("../../middlewares/validateReq
 const user_controller_1 = require("./user.controller");
 const user_validation_1 = require("./user.validation");
 const router = express_1.default.Router();
-router.post('/signup', (0, validateRequest_1.default)(user_validation_1.UserValidation.createUserZodSchema), user_controller_1.UserController.createUser);
+/*
+router.post(
+  '/signup',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser
+);
+*/
 router.patch('/:id', (0, validateRequest_1.default)(user_validation_1.UserValidation.updateUserZodSchema), (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), user_controller_1.UserController.updateUser);
 router.get('/:id', (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), user_controller_1.UserController.getSingleUser);
 router.delete('/:id', (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), user_controller_1.UserController.deleteUser);
